9fa62e1a85acb892bc26156b6cd02225078270cb
Amit Kumar <amitkumar790194@gmail.com>
2024-12-01 00:23:38 +0530

    basic ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import { Excalidraw, Footer, MainMenu, Sidebar } from "@excalidraw/excalidraw";
+import { useState } from "react";
+// import { isLocalLink } from "@excalidraw/excalidraw/types/data/url";
+// import "@excalidraw/excalidraw/dist/excalidraw.min.css"; // Import styles
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  const UIOptions = {
+    canvasActions: {
+      changeViewBackgroundColor: true,
+      clearCanvas: true,
+      loadScene: true,
+    },
+  };
+  const [docked, setDocked] = useState(false);
+
+  return (
+    <div style={{ height: "98vh" }}>
+      <Excalidraw UIOptions={UIOptions}>
+        <MainMenu>
+          <MainMenu.DefaultItems.LoadScene />
+          <MainMenu.DefaultItems.Export />
+          <MainMenu.DefaultItems.SaveAsImage />
+          <MainMenu.DefaultItems.ClearCanvas />
+          <MainMenu.DefaultItems.ToggleTheme />
+          <MainMenu.DefaultItems.ChangeCanvasBackground />
+        </MainMenu>
+        <Sidebar name="custom" docked={docked} onDock={setDocked}>
+          <Sidebar.Header />
+          <Sidebar.Tabs style={{ padding: "0.5rem" }}>
+            <Sidebar.Tab tab="one">Tab one!</Sidebar.Tab>
+            <Sidebar.Tab tab="two">Tab two!</Sidebar.Tab>
+            <Sidebar.TabTriggers>
+              <Sidebar.TabTrigger tab="one">One</Sidebar.TabTrigger>
+              <Sidebar.TabTrigger tab="two">Two</Sidebar.TabTrigger>
+            </Sidebar.TabTriggers>
+          </Sidebar.Tabs>
+        </Sidebar>
+
+        <Footer>
+          <Sidebar.Trigger
+            name="custom"
+            tab="one"
+            style={{
+              marginLeft: "0.5rem",
+              background: "#70b1ec",
+              color: "white",
+            }}
+          >
+            Toggle Custom Sidebar
+          </Sidebar.Trigger>
+        </Footer>
+      </Excalidraw>
+    </div>
+  );
+}
 
 export default App;
